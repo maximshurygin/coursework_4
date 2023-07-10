6a2b0d328244cb407ef648cdc8bc8dc33c74a34f
Maxim Shurygin <maximys2142@mail.ru>
2023-07-10 14:23:56 +0400

    added show_top_vacancy function

--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,19 @@ def show_vacancy():
 
     for x in vacancies:
         print(x)
+
+
+def show_top_vacancy():
+    """Возвращает список top-5 вакансий"""
+    with open(FILENAME, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    vacancies = [Vacancy(line["name"], line["firm"], line["salary from"],
+                         line["salary to"], line["url"], line["area"]) for line in data]
+    top_vacancies = sorted(vacancies, key=lambda x: x.salary_from, reverse=True)[:5]
+
+    if len(top_vacancies) >= 5:
+        print('Топ-5 вакансий:\n')
+        for x in top_vacancies[:5]:
+            print(x)
+    else:
+        print('Недостаточно вакансий для отображения топ-5\n')
