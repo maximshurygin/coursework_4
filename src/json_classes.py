ffcab01c0e61ff5f7863bb991e61161272508c65
Maxim Shurygin <maximys2142@mail.ru>
2023-07-10 14:20:27 +0400

    added JSONSaver class

--- src/json_classes.py
+++ src/json_classes.py
@@ -17,3 +17,31 @@ class JSON(ABC):
     def delete_vacancy(self, x):
         pass
 
+
+class JSONSaver(JSON):
+
+    def save_json(self, api_object):
+        """Записывает в JSON-файл найденные вакансии"""
+        object_list = []
+        for vacancy in api_object.get_vacancies():
+            object_list.append(vacancy)
+
+        with open(self.FILENAME, 'w', encoding='utf-8') as file:
+            json.dump(object_list, file, indent=2, ensure_ascii=False)
+
+    def get_vacancy_by_salary(self, salary_from):
+        """Возвращает список вакансий соответствующих указанной минимальной з/п"""
+        vacancy_list = []
+        with open(self.FILENAME, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            for x in data:
+                if x['salary from']:
+                    if x['salary from'] >= salary_from:
+                        vacancy_list.append(x)
+
+        return vacancy_list
+
+    def delete_vacancy(self, new_data):
+        """Перезаписывает  JSON-файл оставляя только отсортированные вакансии"""
+        with open(self.FILENAME, 'w', encoding='utf-8') as file:
+            json.dump(new_data, file, indent=2, ensure_ascii=False)
